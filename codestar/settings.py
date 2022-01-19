0e2099363fdb8fe569a0befa634e6b018072ff13
Ion71229 <ionsimion279@gmail.com>
2022-01-19 15:07:47 +0000

    deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar2022.herokyapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestart2022.herokuapp.com', 'localhost']
 
 
 # Application definition
